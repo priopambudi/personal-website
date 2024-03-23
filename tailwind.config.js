9325711c282dcc3b39d065271e1170ae01029b2c
priopambudi <prio@vcgamers.com>
2024-03-23 14:54:57 +0700

    header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '0',
+        lg: '0',
+        xl: '0',
+        '2xl': '0',
+      },
+    },
     extend: {},
   },
   plugins: [],
